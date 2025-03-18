db03aeda678fdd3cbc34a927c630bd824fe6d75a
joewadk <jkabir000@citymail.cuny.edu>
2025-03-18 00:22:32 -0400

    test: e2e test from scanner to db insert completed.

---
+++ scanner/e2e_scanner2db.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) #add the parent directory to the path
+
+from scanner.model import extract_text_from_label
+from recommend.recommend2db import recommend2db
+
+def scan_and_recommend(file_path):
+    # Extract text from the label
+    text = extract_text_from_label(file_path)
+    
+    # Recommend times based on the extracted text
+    recommended_times = recommend2db(text)
+    
+    return recommended_times
+#file_path = os.path.join(os.path.dirname(__file__), "images", "test_description.png") #file path for the test image
+#recommended_times = scan_and_recommend(file_path)
+#print(recommended_times)
---
+++ tests/test_e2e_scan2db.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) #add the parent directory to the path
+from scanner.e2e_scanner2db import scan_and_recommend
+
+def test_scan_and_recommend():
+    file_path = os.path.join(os.path.dirname(__file__), "..", "scanner", "images", "test_description.png") #file path for the test image
+    recommended_times = scan_and_recommend(file_path)
+    assert recommended_times == [["06:00", "18:00"]]  # Replace with the expected output
+
+
