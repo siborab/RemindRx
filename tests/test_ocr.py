b57f8147d919cf766c7f7044de6a0a0f85497f39
Abrar Habib <abrarhabib285@gmail.com>
2025-04-06 12:22:33 -0400

    fix: make unit tests not rely on filesystem

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,33 +1,53 @@
-import pytest
 import os
+import pytest
+import tempfile
+from PIL import Image, ImageDraw, ImageFont
 from scanner.model import extract_text_from_label
 
 # Get the absolute path to the tests directory
-TEST_ASSETS_DIR = os.path.join(os.path.dirname(__file__), "assets")
+# TEST_ASSETS_DIR = os.path.join(os.path.dirname(__file__), "assets")
+
+def create_image(text=None, color=(255,255,255)) -> str:
+    image = Image.new("RGB", (200,100), color=color)
+    
+    if text:
+        draw = ImageDraw.Draw(image)
+        try:
+            font = ImageFont.load_default()
+        except IOError:
+            font = None
+        
+        draw.text((10,40), text, fill=(0,0,0), font=font)
+        
+    temp_file = tempfile.NamedTemporaryFile(suffix=".jpg", delete=False)
+    image.save(temp_file.name)
+    temp_file.close()
+    return temp_file.name
+
 
 def test_valid_label():
-    image_path = os.path.join(TEST_ASSETS_DIR, "nothing.jpg")
+    image_path = create_image("Nothing")
     detected_text = extract_text_from_label(image_path)
+    os.remove(image_path)
     assert isinstance(detected_text, str)
     assert detected_text.lower() == "nothing"
 
 def test_empty_image():
-    image_path = os.path.join(TEST_ASSETS_DIR, "black.jpg")
+    image_path = create_image()
     detected_text = extract_text_from_label(image_path)
+    os.remove(image_path)
     assert detected_text == ""
 
 def test_non_existant_file():
-    image_path = os.path.join(TEST_ASSETS_DIR, "doesnotexist.jpg")
     with pytest.raises(FileNotFoundError):
-        extract_text_from_label(image_path)
+        extract_text_from_label("/some/random/nonexistant/file.jpg")
 
 def test_invalid_image_format():
-    invalid_file = os.path.join(TEST_ASSETS_DIR, "not_an_image.txt")
-    
-    with open(invalid_file, "w+") as f:
+    with tempfile.NamedTemporaryFile(suffix=".txt", delete=False, mode="w") as f:
         f.write("This is not an image")
+        invalid_file_path = f.name
 
     with pytest.raises(ValueError):
-        extract_text_from_label(invalid_file)
+        extract_text_from_label(invalid_file_path)
 
-    os.remove(invalid_file)
+    os.remove(invalid_file_path)
