66c2a046c4368362944a63e24e127b1db0c2ad0a
IshmamF <ishmamf2003@gmail.com>
2025-04-06 01:00:42 -0400

    removed generated workflow from playwright i think

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -2,9 +2,11 @@ export default function Navbar() {
 
     return (
         <div>
-            <ul>
+            <ul className="flex space-x-5">
                 <li>Home</li>
                 <li>Pills</li>
+                <li>Sign In</li>
+                <li>Logout</li>
             </ul>
         </div>
     )
