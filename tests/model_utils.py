cf2004f2fbf55b733a6fb1a8fd49cfc82d943919
Abrar Habib <86024593+dddictionary@users.noreply.github.com>
2025-04-28 23:24:09 -0400

    Update model_utils.py

--- tests/model_utils.py
+++ tests/model_utils.py
@@ -8,9 +8,10 @@ def create_high_quality_test_image(text=None, color=(255,255,255)) -> io.BytesIO
 
     try:
         # Use a real font
-        font_path = "./assets/DejaVuSans-Bold.ttf"  # Adjust if needed
+        font_path = os.path.join(os.path.dirname(__file__), "assets", "DejaVuSans-Bold.ttf")
         font = ImageFont.truetype(font_path, size=48)  # Large font size
     except IOError:
+        print("Failed to load font:", e)
         font = ImageFont.load_default()
 
     if text:
