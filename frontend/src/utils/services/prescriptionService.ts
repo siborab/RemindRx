561cd649d2c64fa4c9de0d67165bf38307dc0c44
Ivan Chen <ivanchen498@gmail.com>
2025-05-03 17:57:21 -0400

    removed unused files and directories related to db

--- frontend/src/utils/services/prescriptionService.ts
+++
@@ -1,9 +0,0 @@
-import PrescriptionRepository from "../repository/prescriptionRepository"
-
-class PrescriptionService {
-    private prescriptions: PrescriptionRepository;
-
-    constructor(prescriptions: PrescriptionRepository) {
-        this.prescriptions = prescriptions
-    }
-}
