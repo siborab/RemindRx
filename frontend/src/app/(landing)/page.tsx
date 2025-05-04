87793cde99956a53fb961268afe11c40f50432ac
Ivan Chen <ivanchen498@gmail.com>
2025-05-04 17:06:04 -0400

    fix: authentication flow, initially lead to landing page

--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -4,13 +4,36 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { useAuth } from "@/app/providers/authprovider"
 import { useRouter } from "next/navigation"
-import Signin from "../(auth)/signin/page"
 import HomePage from "../(private)/home/page"
 
 
-export default function Home() {
+export default function Landing() {
   const { isAuthenticated } = useAuth();
   return (isAuthenticated ?
-    <HomePage /> : <Signin />
+    <HomePage /> :
+    <div >
+      <div className="w-full py-12 md:py-24 lg:py-32 xl:py-48 px-4 md:px-6">
+        <div className="flex flex-col justify-center space-y-4">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold sm:text-5xl xl:text-6xl">
+              Never Miss a Medication Again
+            </h1>
+            <p className="max-w-[600px] md:text-xl">
+              Our solution helps you stay on top of your prescriptions.
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 min-[400px]:flex-row">
+            <Link href="/signin">
+              <Button size="lg">Sign In</Button>
+            </Link>
+            <Link href="/signup">
+              <Button size="lg" variant="outline">
+                Create Account
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
