005beab79262f5ee0c7adab9263b3a5ae8985cb6
joewadk <jkabir000@citymail.cuny.edu>
2025-05-04 21:25:58 -0400

    feat: added helper functions to get medications for specific time-of-day windows

--- server/app/api/models.py
+++ server/app/api/models.py
@@ -2,7 +2,7 @@ import os
 from flask import Blueprint, request, jsonify
 from flask_cors import CORS
 from scanner.model import extract_text_from_label
-from .utils import upload, get_db, get_email, get_patient
+from .utils import upload, get_db, get_email, get_patient, get_morning_medications, get_afternoon_medications , get_evening_medications
 import tempfile
 from recommend.recommend2db import recommend2db
 
@@ -53,3 +53,24 @@ def test2():
     db = get_db()
 
     return jsonify({"Email":get_email(db, 6)}) #use the get_email function to get the email of the user with a given patient_id 
+
+
+
+#note from jawad- these are methods to get the patient's medications for a given time of day. mainly just debug for the helper functions
+@models_api.route("/get_morning", methods=["GET"])
+def get_morning():
+    db = get_db()
+    patient_id = 6
+    return jsonify({"Here's the following morning medications we got": get_morning_medications(db, patient_id)}) #get the morning medications for a given patient_id
+
+@models_api.route("/get_afternoon", methods=["GET"])
+def get_afternoon():
+    db = get_db()
+    patient_id = 6
+    return jsonify({"Here's the following afternoon medications we got": get_afternoon_medications(db, patient_id)}) #get the afternoon medications for a given patient_id
+
+@models_api.route("/get_evening", methods=["GET"])
+def get_evening():
+    db = get_db()
+    patient_id = 6
+    return jsonify({"Here's the following evening medications we got": get_evening_medications(db, patient_id)}) #get the evening medications for a given patient_id
